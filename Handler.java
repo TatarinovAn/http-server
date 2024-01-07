fc3ac7303517898d83543e0bfb972932f6814dd2
TatarinovAn <gitaline@bk.ru>
2024-01-08 01:10:49 +0300

    add handler

--- Handler.java
+++
@@ -1,8 +0,0 @@
-package ru.netology;
-
-import java.io.BufferedOutputStream;
-
-@FunctionalInterface
-public interface Handler {
-    public void handler(Request request, BufferedOutputStream bufferedOutputStream);
-}
